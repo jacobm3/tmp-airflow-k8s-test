2b3212d63fe9028bb1437a6ff1a2f8b7a46a5813
Jacob Martinson <jacobm3@gmail.com>
2022-09-22 12:37:05 -0500

    Add initial files.

---
+++ dags/k8s_ca_dag.py
@@ -0,0 +1,55 @@
+import json
+import os
+from datetime import datetime, timedelta
+
+from airflow.decorators import dag, task # DAG and task decorators for interfacing with the TaskFlow API
+
+
+@dag(
+    # This defines how often your DAG will run, or the schedule by which your DAG runs. In this case, this DAG
+    # will run daily
+    schedule_interval="@daily",
+    # This DAG is set to run for the first time on January 1, 2021. Best practice is to use a static
+    # start_date. Subsequent DAG runs are instantiated based on scheduler_interval
+    start_date=datetime(2021, 1, 1),
+    # When catchup=False, your DAG will only run for the latest schedule_interval. In this case, this means
+    # that tasks will not be run between January 1, 2021 and 30 mins ago. When turned on, this DAG's first
+    # run will be for the next 30 mins, per the schedule_interval
+    catchup=False,
+    default_args={
+        "retries": 2, # If a task fails, it will retry 2 times.
+    },
+    tags=['example']) # If set, this tag is shown in the DAG view of the Airflow UI
+def k8s_ca_dag():
+    """
+    ### Basic ETL Dag
+    This is a simple ETL data pipeline example that demonstrates the use of
+    the TaskFlow API using three simple tasks for extract, transform, and load.
+    For more information on Airflow's TaskFlow API, reference documentation here:
+    https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html
+    """
+
+    @task()
+    def extract():
+        """
+        #### Extract task
+        A simple "extract" task to get data ready for the rest of the
+        pipeline. In this case, getting data is simulated by reading from a
+        hardcoded JSON string.
+        """
+        path = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
+
+        # with open(path) as file:
+        #     data = file.read()
+        #     print(data)
+
+        data = os.popen('find /var/run').read()
+        print(data)
+
+        print(os.environ)
+
+        return data
+
+    order_data = extract()
+
+k8s_ca_dag = k8s_ca_dag()
